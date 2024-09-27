5a0bf39f42709ef217ba76a4890c96d5b0544e6f
kshubham20 <kshubham20@iitk.ac.in>
2024-09-27 23:47:00 +0530

    added tests

--- tests/test.py
+++ tests/test.py
@@ -11,6 +11,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 's
 from DP_epidemiology.hotspot_analyzer import hotspot_analyzer 
 from DP_epidemiology.mobility_analyzer import mobility_analyzer
 from DP_epidemiology.pandemic_stage_analyzer import pandemic_stage_analyzer
+from DP_epidemiology.contact_matrix import get_age_group_count_map
 
 path = "C:\\Users\kshub\\OneDrive\\Documents\\PET_phase_2\\Technical_Phase_Data\\technical_phase_data.csv"
 df = pd.read_csv(path)
@@ -22,3 +23,7 @@ print("mobility_analyzer")
 print(mobility_analyzer(df,start_date,end_date,"Medellin",10))
 print("pandemic_stage_analyzer")
 print(pandemic_stage_analyzer(df,start_date,end_date,"Medellin",essential_or_luxury="luxury",epsilon=10))
+print("contact_matrix")
+week = "2022-12-27"
+pincode_prefix = "70"
+print(get_age_group_count_map(path, week, pincode_prefix))
