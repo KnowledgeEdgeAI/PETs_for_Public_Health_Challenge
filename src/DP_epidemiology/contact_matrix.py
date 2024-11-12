d86472bf0ec07a8ebe8d3c37739999b1be9aedab
Milan anand raj <84122339+manandraj20@users.noreply.github.com>
2024-11-12 08:18:33 -0500

    estimating consumption distribution

--- src/DP_epidemiology/contact_matrix.py
+++ src/DP_epidemiology/contact_matrix.py
@@ -14,8 +14,14 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 time_col = "date"
 city_col = "city"
-# upper bound for the nb_transactions column
+txn_channel_col = "transaction_type"
+category_col = "merch_category"
+postal_code_col = "merch_postal_code"
+num_txns_col = "nb_transactions"
+
+# upper bound on nb_transactions for any merhcant
 UPPER_BOUND = 600
+
 
 def validate_input_data(df, age_groups, consumption_distribution, start_date: datetime, end_date: datetime, city: str):
     # check city exists in the data
@@ -34,15 +40,12 @@ def validate_input_data(df, age_groups, consumption_distribution, start_date: da
         if category not in merch_categories:
             raise ValueError(f"Category {category} does not exist in the data")
 
-    
-def get_age_group_count_map(df, age_groups, consumption_distribution, start_date: datetime, end_date: datetime, city: str, epsilon: float = 1.0):
 
-    validate_input_data(df, age_groups, consumption_distribution, start_date, end_date, city)
-
+def get_private_counts(df, categories, start_date: datetime, end_date: datetime, city: str, epsilon: float = 1.0):
     t_pre = (
         make_preprocess_location()
-        >> make_truncate_time(start_date, end_date, "date")
-        >> make_filter_rows("transaction_type", "OFFLINE")
+        >> make_truncate_time(start_date, end_date, time_col=time_col)
+        >> make_filter_rows(txn_channel_col, "OFFLINE")
         >> make_filter_rows(city_col, city)
     )
     number_of_timesteps = 1 if end_date == start_date else (
@@ -50,7 +53,7 @@ def get_age_group_count_map(df, age_groups, consumption_distribution, start_date
     input_space = dp.vector_domain(
         dp.atom_domain(T=int)), dp.symmetric_distance()
     df_new = t_pre(df)
-    zip_code_list = df_new["merch_postal_code"].unique().astype(int)
+    zip_code_list = df_new[postal_code_col].unique().astype(int)
     count_meas = input_space >> dp.t.then_count() >> dp.m.then_laplace(
         (3 * number_of_timesteps)/epsilon)
     dp_count = count_meas(zip_code_list)
@@ -58,8 +61,7 @@ def get_age_group_count_map(df, age_groups, consumption_distribution, start_date
     # Calculate the average number of transactions in each zip code for each merchant category during the given week.
     nb_transactions_avg_count_map = {}
 
-    
-    for category in consumption_distribution.keys():
+    for category in categories:
         m_count = (
             t_pre
             # TODO: The scale has to be equal to bound/epsilon, which can be equal to the mean itself in cases where number of entries
@@ -68,17 +70,37 @@ def get_age_group_count_map(df, age_groups, consumption_distribution, start_date
             # If needed, take epsilon to be 10 for the category where bound is maximum of all
             >> make_private_nb_transactions_avg_count(merch_category=category, upper_bound=UPPER_BOUND, dp_dataset_size=dp_count, scale=(3*UPPER_BOUND*number_of_timesteps)/epsilon)
         )
-        # print(category,":", m_count(df))
         nb_transactions_avg_count_map[category] = m_count(df)
+
+    return nb_transactions_avg_count_map
+
+
+def get_age_group_count_map(df, age_groups, consumption_distribution, start_date: datetime, end_date: datetime, city: str, epsilon: float = 1.0):
+
+    validate_input_data(
+        df, age_groups, consumption_distribution, start_date, end_date, city)
+
+    nb_transactions_avg_count_map = get_private_counts(
+        df, consumption_distribution.keys(), start_date, end_date, city, epsilon)
 
     # calculate age group to avg count of members from that age group
     age_group_count_map = {}
     for age_group in age_groups:
         age_group_count_map[age_group] = max(np.sum([(proportion_list[age_groups.index(age_group)]/100)*nb_transactions_avg_count_map[category]
-                                                 for category, proportion_list in consumption_distribution.items()]), 0)
-   
+                                                     for category, proportion_list in consumption_distribution.items()]), 0)
 
     return age_group_count_map
+
+# get average contact matrix for a group of cities
+def get_contact_matrix(counts_per_city, population_distribution, fractions_offline):
+    age_bins = np.array(counts_per_city)
+    num_cities = len(counts_per_city)
+
+    contact_matrix = np.sum([np.matmul(np.reshape(
+        x, (-1, 1)), np.reshape(1 / x, (1, -1))) for x in age_bins], axis=0) / num_cities
+    contact_matrix = contact_matrix*(population_distribution*fractions_offline)
+    contact_matrix = (contact_matrix + np.transpose(contact_matrix))/2
+    return contact_matrix/population_distribution
 
 
 def get_contact_matrix(sample_distribution, population_distribution):
@@ -93,6 +115,7 @@ def get_contact_matrix(sample_distribution, population_distribution):
         for j in range(size):
             M[i][j] = C[i][j]/sample_distribution[j]
 
+    # The next steps is done to make sure the total contacts made between different age groups is symmetric
     T = np.zeros((size, size))
     for i in range(size):
         for j in range(size):
@@ -104,20 +127,21 @@ def get_contact_matrix(sample_distribution, population_distribution):
     for i in range(size):
         for j in range(size):
             F[i][j] = T[i][j]/population_distribution[j]
-
-    return 2.8*F
+    return F
 
 
 def get_pearson_similarity(contact_matrix, Ground_truth_contact_matrix):
-    Ground_truth_contact_matrix = [
-        [3.91155, 3.84487, 3.32271],
-        [2.12514, 2.77554, 3.19146],
-        [2.15473, 2.34549, 2.70081]
-    ]
+    # Example ground truth matrix for age groups 20-30, 30-40, 40-50
+    # Ground_truth_contact_matrix = [
+    #     [3.91155, 3.84487, 3.32271],
+    #     [2.12514, 2.77554, 3.19146],
+    #     [2.15473, 2.34549, 2.70081]
+    # ]
     # Calculate the Pearson similarity between the ground truth contact matrix and the contact matrix
     pearson_similarity = np.corrcoef(np.array(
         Ground_truth_contact_matrix).flatten(), np.array(contact_matrix).flatten())[0, 1]
     return pearson_similarity
+
 
 def plot_difference(A, B):
     difference = A - B
@@ -129,4 +153,3 @@ def plot_difference(A, B):
     plt.xlabel('Index i2')
     plt.ylabel('Index i1')
     plt.show()
-
